efb7a1b6ebcf1867d44ff0c115175684afbbc713
Joshua Lawson <jacklegjosh@gmail.com>
2017-08-17 11:01:59 -0400

    initial commit, basic structure

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Put this anywhere; normal scope rules will apply
+let testModule = require('./module'); // import module.js
+
+window.addEventListener('load', function () {
+    console.log('ready to rock');
+});
