ccc7689ee20abfa3311dfdf368eb8b37a32b543a
Joshua Lawson <jacklegjosh@gmail.com>
2017-09-17 14:43:32 -0400

    many small tweaks, flirting with an image map

--- js/app.js
+++ js/app.js
@@ -3,4 +3,18 @@ let testModule = require('./module'); // import module.js
 
 window.addEventListener('load', function () {
     console.log('ready to rock');
+
+    // let housePic = document.querySelector('#house-pic');
+    
+    // let letters = document.querySelectorAll('p.clickable-letter');
+    
+    // // When the first letter gets clicked, run the function that's being passed
+    // // as the second parameter.
+    // letters[0].addEventListener('click', function () {
+    //   housePic.src = '/house-1.png';
+    // });
+    
+    // letters[1].addEventListener('click', function () {
+    //   housePic.src = '/house-2.png';
+    // });
 });
